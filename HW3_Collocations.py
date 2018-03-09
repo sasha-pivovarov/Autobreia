3de606d3d08aa3a19223803d106c1e62e9569e70
sasha-pivovarov <cantwork.pivovarov@gmail.com>
2018-03-10 02:34:15 +0300

    ДЗ 3

--- HW3_Collocations.py
+++ HW3_Collocations.py
@@ -11,16 +11,23 @@ corpus = pd.read_csv("court-V-N.csv", header=None)
 measures = BigramAssocMeasures()
 tagger = lambda x: (x, analyzer.parse(x.lower().strip())[0].tag.POS)
 tagged_corpus = corpus.applymap(tagger).drop(0, axis=1)
-
+with open("gold_standard.txt", "r") as io:
+    standard = [tuple(x.split()) for x in io.readlines()]
 wfd = FreqDist(tagged_corpus.values.flatten())
 bfd = FreqDist(bigrams(tagged_corpus.values.flatten()))
 finder_1 = BigramCollocationFinder(wfd, bfd)
 
-filter = lambda x: [y for y in x if y[0][0][1] == "INFN"]
+filter = lambda x: [tuple(z[0] for z in y[0]) for y in x if y[0][0][1] == "INFN"]
+
 scored_pmi = filter(finder_1.score_ngrams(measures.pmi))
 scored_student = filter(finder_1.score_ngrams(measures.student_t))
-scored_chisquared = filter(finder_1.score_ngrams(measures.chi_sq))
+pmi_top = scored_pmi[:10]
+student_top = scored_student[:10]
 
+for name, top in [("pmi_top10.txt", pmi_top), ("student_top10.txt", student_top)]:
+    with open(name, "w") as io:
+        joined = [" ".join(x) + "\n" for x in top]
+        io.writelines(joined)
 
-
+print(spearman.spearman_correlation(pmi_top, student_top))
 print("Done")
